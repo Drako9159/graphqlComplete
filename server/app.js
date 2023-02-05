8d60929f50df707e0eb1a2f95bd350400ad63ea8
drako9159 <drako9159@gmail.com>
2023-02-05 12:21:08 -0600

    add estructure

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors"
+import http from "http"
+
+export async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app)
+  const server = new ApolloServer({
+    typeDefs: ``,
+    resolvers: () => {},
+  });
+  await server.start();
+
+  app.use("/graphql", cors(), express.json(), expressMiddleware);
+  await new Promise((resolve) => httpServer.listen({
+    port: 4000
+  }))
+}
